6a20644bd197781f1467916d1b3001b122b7c090
Pawan sharma <pawansh803@gmail.com>
2024-09-28 23:05:44 +0530

    Discuss Project

--- discuss/index.php
+++ discuss/index.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
   <title>Discuss Project</title>
+  <?php include('./client/commonFiles.php') ?>
 </head>
 <body>
-  
+  <?php include('./client/header.php') ?>
 </body>
 </html>
