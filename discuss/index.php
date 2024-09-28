b25c76fe00e983bd5f2ee3e19feb65b55b9b027c
Pawan sharma <pawansh803@gmail.com>
2024-09-28 22:18:01 +0530

    php project

--- discuss/index.php
+++ discuss/index.php
@@ -2,16 +2,5 @@
 
 echo "Discuss Project";
 
-echo "this is a new project";
-
-echo "let's start the project";
-
-echo "we are creating a Q/A website";
-
-echo "we are creating a login logout feature for user";
-
-echo "we will add related questions also";
-
-echo "after login you can add questions also";
 
 ?>
