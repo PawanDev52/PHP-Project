193db1542a43fdbce1af9cb7811908728241de84
Pawan sharma <pawansh803@gmail.com>
2024-10-04 16:47:38 +0530

    php project

--- discuss/client/signup.php
+++ discuss/client/signup.php
@@ -2,10 +2,9 @@
     <h1>Signup</h1>
 
     <form>
-        <div class="sm-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <div class="col-6">
+            <label for="username" class="form-label">User Name</label>
+            <input type="text" class="form-control" id="username">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
