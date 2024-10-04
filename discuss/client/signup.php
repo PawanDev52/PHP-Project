0e68f79f05fca922626616509130d005bd5f77b8
Pawan sharma <pawansh803@gmail.com>
2024-10-04 16:54:39 +0530

    php project

--- discuss/client/signup.php
+++ discuss/client/signup.php
@@ -4,14 +4,24 @@
     <form>
         <div class="col-6 offset-sm-3">
             <label for="username" class="form-label">User Name</label>
-            <input type="text" class="form-control" id="username" placeholder="Enter User Name">
+            <input type="text" class="form-control" id="username" placeholder="Enter user Name">
         </div>
 
         <div class="col-6 offset-sm-3">
-            <label for="username" class="form-label">User Name</label>
-            <input type="text" class="form-control" id="username" placeholder="Enter User Name">
+            <label for="email" class="form-label">User Email</label>
+            <input type="text" class="form-control" id="email" placeholder="Enter user email">
         </div>
-        
+
+        <div class="col-6 offset-sm-3">
+            <label for="password" class="form-label">User Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Enter user password">
+        </div>
+
+        <div class="col-6 offset-sm-3">
+            <label for="address" class="form-label">User Address</label>
+            <input type="text" class="form-control" id="address" placeholder="Enter user address">
+        </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
