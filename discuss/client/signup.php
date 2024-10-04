03f06c1d013a64c19966ec461a03bd72dc86b0e0
Pawan sharma <pawansh803@gmail.com>
2024-10-04 16:44:02 +0530

    php project

--- discuss/client/signup.php
+++ discuss/client/signup.php
@@ -1,6 +1,12 @@
 <div class="container">
     <h1>Signup</h1>
-</div>
-<?php
 
-?>
+    <form>
+        <div class="sm-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
