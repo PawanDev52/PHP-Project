c6c30ce219db84fce91851bb7feeaaa58e0e6576
Pawan sharma <pawansh803@gmail.com>
2024-10-04 16:57:17 +0530

    php project

--- discuss/client/signup.php
+++ discuss/client/signup.php
@@ -1,27 +1,29 @@
 <div class="container">
-    <h1>Signup</h1>
+  <h1>Signup</h1>
 
-    <form>
-        <div class="col-6 offset-sm-3">
-            <label for="username" class="form-label">User Name</label>
-            <input type="text" class="form-control" id="username" placeholder="Enter user Name">
-        </div>
+  <form>
+    <div class="col-6 offset-sm-3">
+      <label for="username" class="form-label">User Name</label>
+      <input type="text" class="form-control" id="username" placeholder="Enter user Name">
+    </div>
 
-        <div class="col-6 offset-sm-3">
-            <label for="email" class="form-label">User Email</label>
-            <input type="text" class="form-control" id="email" placeholder="Enter user email">
-        </div>
+    <div class="col-6 offset-sm-3">
+      <label for="email" class="form-label">User Email</label>
+      <input type="text" class="form-control" id="email" placeholder="Enter user email">
+    </div>
 
-        <div class="col-6 offset-sm-3">
-            <label for="password" class="form-label">User Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Enter user password">
-        </div>
+    <div class="col-6 offset-sm-3">
+      <label for="password" class="form-label">User Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Enter user password">
+    </div>
 
-        <div class="col-6 offset-sm-3">
-            <label for="address" class="form-label">User Address</label>
-            <input type="text" class="form-control" id="address" placeholder="Enter user address">
-        </div>
+    <div class="col-6 offset-sm-3">
+      <label for="address" class="form-label">User Address</label>
+      <input type="text" class="form-control" id="address" placeholder="Enter user address">
+    </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="col-6 offset-sm-3">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
 </div>
