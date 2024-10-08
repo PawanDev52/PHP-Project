ed32ae1ad69129f2a170a14819b4e0c1d3194e3d
Pawan sharma <pawansh803@gmail.com>
2024-10-08 22:57:56 +0530

    php project

--- discuss/server/requests.php
+++ discuss/server/requests.php
@@ -1,4 +1,10 @@
 <?php
+// signup form 
+if(isset($_POST['signup'])){
+    echo "User name is " . $_POST['username'] . "<br>";
+    echo "User email is " . $_POST['email'] . "<br>";
+    echo "User password is " . $_POST['password'] . "<br>";
+    echo "User address is " . $_POST['address'] . "<br>";
+}
 
-print_r($_POST);
 ?>
