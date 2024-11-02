fe3aafb14de9b6324a2c508e38d788a22f1a2326
Pawan sharma <pawansh803@gmail.com>
2024-11-02 19:34:38 +0530

    php project

--- client/header.php
+++ client/header.php
@@ -1,4 +1,23 @@
-<?php
-
-
-?>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./public/logo.png" alt="logo">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Category</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link" href="#">Latest Questions</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
