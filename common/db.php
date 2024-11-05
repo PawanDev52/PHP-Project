3b249439f3229c2b8fc4264fb13fd9ae9e4a65e0
Pawan sharma <pawansh803@gmail.com>
2024-11-05 22:49:17 +0530

    php project

--- common/db.php
+++ common/db.php
@@ -6,6 +6,11 @@ $username = "root";
 $password = "";
 $database = "discuss";
 
-echo "db file";
+$conn = new mysqli($host, $username, $password, $database);
 
+if ($conn->connect_error) {
+    die("Not connected with db " . $conn->connect_error);
+}
+
+echo "Database connected";
 ?>
