09032a31c56d4f1834ee1ddd105e6fab87213d48
Pawan sharma <pawansh803@gmail.com>
2024-11-05 22:22:44 +0530

    php file

--- client/signup.php
+++ client/signup.php
@@ -3,26 +3,26 @@
   <form>
   <div class="col-6 offset-sm-3 mb-3">
     <label for="username" class="form-label">User Name</label>
-    <input type="text" class="form-control" id="username" placeholder="Enter user name">
+    <input type="text" name="username" class="form-control" id="username" placeholder="Enter user name">
   </div>
 
   <div class="col-6 offset-sm-3 mb-3">
     <label for="email" class="form-label">User Email</label>
-    <input type="text" class="form-control" id="email" placeholder="Enter user email">
+    <input type="text" name="email" class="form-control" id="email" placeholder="Enter user email">
   </div>
 
   <div class="col-6 offset-sm-3 mb-3">
     <label for="password" class="form-label">User Password</label>
-    <input type="password" class="form-control" id="password" placeholder="Enter user password">
+    <input type="password" name="password" class="form-control" id="password" placeholder="Enter user password">
   </div>
 
   <div class="col-6 offset-sm-3 mb-3">
     <label for="address" class="form-label">User Address</label>
-    <input type="text" class="form-control" id="address" placeholder="Enter user address">
+    <input type="text" name="address" class="form-control" id="address" placeholder="Enter user address">
   </div>
 
   <div class="col-6 offset-sm-3">
-    <button type="submit" class="btn btn-primary">Signup</button>
+    <button type="submit" name="signup" class="btn btn-primary">Signup</button>
   </div>
   
 </form>
