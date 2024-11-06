52679e903d186dcabd0885dbe68c776d29731eb4
Pawan sharma <pawansh803@gmail.com>
2024-11-06 20:53:16 +0530

    php project

--- server/requests.php
+++ server/requests.php
@@ -28,7 +28,19 @@ if (isset($_POST['signup'])) {
 } else if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $username = "";
     $query = "select * from users where email='$email' and password='$password'";
-}
+    $result = $conn->query($query);
 
+    if ($result->num_rows == 1) {
+        foreach ($result as $row) {
+            // print_r($row);
+            $username = $row['username'];
+        }
+        $_SESSION["user"] = ["username" => $username, "email" => $email, "password" => $password];
+        header("location: /phpprojectdiscuss");
+    } else {
+        echo "user not logged in";
+    }
+}
 ?>
