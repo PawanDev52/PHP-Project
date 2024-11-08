4f404c4d8a7a3dc69d67e8e49393fecba07d8432
Pawan sharma <pawansh803@gmail.com>
2024-11-08 15:23:02 +0530

    php project

--- client/ask.php
+++ client/ask.php
@@ -1,4 +1,12 @@
-<?php
-
-
-?>
+<div class="container">
+  <h1 class="text-center mb-3">Ask a Question</h1>
+  <form action="./server/requests.php" method="post">
+    <div class="col-6 offset-sm-3 mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input type="text" name="title" class="form-control" id="title" placeholder="Enter question">
+    </div>
+    <div class="col-6 offset-sm-3">
+      <button type="submit" name="login" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</div>
