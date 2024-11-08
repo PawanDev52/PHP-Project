9cb7094db6a3ef0a5710409241134e9d1e983aa7
Pawan sharma <pawansh803@gmail.com>
2024-11-08 17:14:05 +0530

    php project

--- server/requests.php
+++ server/requests.php
@@ -18,36 +18,53 @@ if (isset($_POST['signup'])) {
     ");
 
     $result = $user->execute();
+    $user->insert_id;
 
     if ($result) {
-        $_SESSION["user"] = ["username" => $username, "email" => $email];
+        $_SESSION["user"] = ["username" => $username, "email" => $email, "user_id" => $user->insert_id];
         header("location: /phpprojectdiscuss");
     } else {
         echo "new user not registered";
     }
-
 } else if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $username = "";
+    $user_id = 0;
+
     $query = "select * from users where email='$email' and password='$password'";
     $result = $conn->query($query);
 
     if ($result->num_rows == 1) {
         foreach ($result as $row) {
             $username = $row['username'];
+            $user_id = $row['id'];
         }
-        $_SESSION["user"] = ["username" => $username, "email" => $email, "password" => $password];
+        $_SESSION["user"] = ["username" => $username, "email" => $email, "password" => $password, "user_id" => $user_id];
         header("location: /phpprojectdiscuss");
     } else {
         echo "user not logged in";
     }
-
 } else if (isset($_GET['logout'])) {
     session_unset();
     header("location: /phpprojectdiscuss");
-} else if(isset($_POST['ask'])){
-    print_r($_POST);
-    print_r($_SESSION);
+} else if (isset($_POST['ask'])) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $category_id = $_POST['category'];
+    $user_id = $_SESSION['user']['user_id'];
+
+    $question = $conn->prepare("Insert into `questions`
+    (`title`, `description`, `category_id`, `user_id`)
+    values('$title', '$description', '$category_id', '$user_id')
+    ");
+
+    $result = $question->execute();
+    $question->insert_id;
+    if ($result) {
+        header("location: /phpprojectdiscuss");
+    } else {
+        echo "Question not added";
+    }
 }
 ?>
