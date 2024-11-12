02726ca0eec34c54d621c8a8dfb17e3cca355a3d
Pawan sharma <pawansh803@gmail.com>
2024-11-12 21:25:45 +0530

    php project

--- client/questions.php
+++ client/questions.php
@@ -1,13 +1,17 @@
 <div class="container">
   <h1 class="text-center">Questions</h1>
+  <div class="col-8">
   <?php
   include("./common/db.php");
   $query = "select * from questions";
   $result = $conn->query($query);
   foreach($result as $row){
     $title = $row['title'];
-    echo "<div class='m-2 p-3 question-list'>$title</div>";
+    echo "<div class='m-2 p-3 question-list'>
+    <h4 class='m-0 p-0'><a href='#'>$title</a></h4>
+    </div>";
 
   }
   ?>
+  </div>
 </div>
