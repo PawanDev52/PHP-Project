658d9c3926d928e75f3ff933a1987519dba6878c
Pawan sharma <pawansh803@gmail.com>
2024-11-13 23:13:47 +0530

    php project

--- client/question-details.php
+++ client/question-details.php
@@ -1,10 +1,17 @@
 <div class="container">
   <h1 class="text-center">Questions</h1>
+  <div class="col-8">
   <?php
   include("./common/db.php");
   $query = "select * from questions where id = $qid";
   $result = $conn->query($query);
   $row = $result->fetch_assoc();
-  print_r($row);
+  
+  echo "<h4 class='mb-3 qsn-dtl'> Question : " . $row['title'] . "</h4>
+  <p class='mb-3'>" . $row['description'] . "</p>";
   ?>
+
+  <textarea class="form-control mb-3" placeholder="Write your answer"></textarea>
+  <button class="btn btn-primary">Submit</button>
+  </div>
 </div>
--- client/questions.php
+++ client/questions.php
@@ -7,9 +7,10 @@
   $result = $conn->query($query);
   foreach($result as $row){
     $title = $row['title'];
+    $qtitle = ucfirst($title);
     $id = $row['id'];
     echo "<div class='m-2 p-3 question-list'>
-    <h4 class='m-0 p-0'><a href='?q-id=$id'>$title</a></h4>
+    <h4 class='m-0 p-0'><a href='?q-id=$id'>$qtitle</a></h4>
     </div>";
 
   }
