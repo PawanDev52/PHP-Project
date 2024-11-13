1431bf39e417be79811f4cdee6e252e5807c94cc
Pawan sharma <pawansh803@gmail.com>
2024-11-13 23:01:15 +0530

    php project

--- client/question-details.php
+++ client/question-details.php
@@ -1,6 +1,10 @@
 <div class="container">
   <h1 class="text-center">Questions</h1>
   <?php
-  
+  include("./common/db.php");
+  $query = "select * from questions where id = $qid";
+  $result = $conn->query($query);
+  $row = $result->fetch_assoc();
+  print_r($row);
   ?>
 </div>
