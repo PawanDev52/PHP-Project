d8adf16aa9ae5a014643fdfb93feeb85ff0d13fa
Pawan sharma <pawansh803@gmail.com>
2024-11-14 17:50:17 +0530

    php project

--- client/categorylist.php
+++ client/categorylist.php
@@ -1,6 +1,16 @@
 <div>
-  <h2 class="text-center">Categories</h2>
+  <h1 class="text-center">Categories</h1>
   <?php
   include('./common/db.php');
+
+  $query = "select * from category";
+  $result = $conn->query($query);
+  foreach ($result as $row) {
+    $name = ucfirst($row['name']);
+    $id = $row['id'];
+    echo "<div class='m-2 p-3 question-list'>
+    <h4 class='m-0 p-0'><a href='?q-id=$id'>$name</a></h4>
+    </div>";
+  }
   ?>
 </div>
