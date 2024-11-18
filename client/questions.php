b637f173c80cc824bc7d15206fc00308c187d621
Pawan sharma <pawansh803@gmail.com>
2024-11-18 17:32:43 +0530

    php project

--- client/questions.php
+++ client/questions.php
@@ -8,6 +8,8 @@
         $query = "select * from questions where category_id=$cid";
       } else if(isset($_GET['u-id'])){
         $query = "select * from questions where user_id=$uid";
+      } else if(isset($_GET['latest'])){
+        $query = "select * from questions order by id desc";
       } else{
         $query = "select * from questions";
       }
